45690062d811aa7d07907f8448f18181c4074627
IgotChuHomie <abdouneoussama592@gmail.com>
2022-04-04 23:29:21 +0000

    second commit

--- playground/views.py
+++ playground/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.http import HttpResponse 
 
-# Create your views here.
+
+
+#Simple request handler 
+'''
+def say_hello(request) :
+    return HttpResponse("hello world")
+'''
+
+
+#simple request handler using templates 
+def say_hello(request) :
+    return render(request,'hello.html',{'name':'oussama'})
--- storefront/settings.py
+++ storefront/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'playground'
+    'playground',
+    'debug_toolbar',
 ]
 
 MIDDLEWARE = [
@@ -48,9 +49,17 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
 ]
 
 ROOT_URLCONF = 'storefront.urls'
+
+
+INTERNAL_IPS = [
+    # ...
+    "127.0.0.1",
+    # ...
+]
 
 TEMPLATES = [
     {
