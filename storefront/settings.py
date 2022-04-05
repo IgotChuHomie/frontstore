b7dcb906d5c4adbd282ed32906e3602cdb95028c
IgotChuHomie <abdouneoussama592@gmail.com>
2022-04-05 13:38:09 +0000

    adding store and tags app to the project

--- storefront/settings.py
+++ storefront/settings.py
@@ -38,7 +38,10 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'playground',
-    'debug_toolbar',
+    'store',
+    'tags'
+
+
 ]
 
 MIDDLEWARE = [
@@ -49,17 +52,14 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'debug_toolbar.middleware.DebugToolbarMiddleware',
+
 ]
 
 ROOT_URLCONF = 'storefront.urls'
 
 
-INTERNAL_IPS = [
-    # ...
-    "127.0.0.1",
-    # ...
-]
+
+
 
 TEMPLATES = [
     {
