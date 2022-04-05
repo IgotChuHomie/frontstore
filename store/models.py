146c9fb4aeb8b1f26fdc8c96cea48405823f1b83
IgotChuHomie <abdouneoussama592@gmail.com>
2022-04-05 15:25:45 +0000

    adding tag model and taggeditem

--- store/models.py
+++ store/models.py
@@ -8,7 +8,7 @@ from django.db import models
 class Promotion(models.Model) : 
     description = models.CharField(max_length=255)
     discount = models.FloatField()
-    #products 
+    featured_product = models.ForeignKey('Product',on_delete=models.SET_NULL,null=True,related_name="+")
 
 class Collection(models.Model):
     title = models.CharField(max_length=255)
--- storefront/settings.py
+++ storefront/settings.py
@@ -39,9 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'playground',
     'store',
-    'tags'
-
-
+    'tags',
 ]
 
 MIDDLEWARE = [
