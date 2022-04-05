9bd53f115e6129fe519c9205385e652e91a9a235
IgotChuHomie <abdouneoussama592@gmail.com>
2022-04-05 13:58:09 +0000

    modifying Customer model adding membership field

--- store/models.py
+++ store/models.py
@@ -11,10 +11,24 @@ class Product(models.Model) :
 
 
 class Customer(models.Model) :
+    MEMBERSHIP_BRONZE = 'B' 
+    MEMBERSHIP_SILVER = 'S'
+    MEMBERSHIP_GOLD = 'G'
+    MEMBERSHIP_CHOICES = [
+        (MEMBERSHIP_BRONZE,'Bronze'),
+        (MEMBERSHIP_SILVER,'Silver'),
+        (MEMBERSHIP_GOLD,'Gold')
+        ]
+    
     first_name = models.CharField(max_length=255)
     last_name = models.CharField(max_length=255)
     email = models.EmailField(unique=True)
     phone = models.CharField(max_length=255)
     birth_date = models.DateField(null=True)
+    membership = models.CharField(max_length=255,choices=MEMBERSHIP_CHOICES,default=MEMBERSHIP_BRONZE)
+    
+
+
+    
     
     
