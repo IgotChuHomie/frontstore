7b57c9e1335d4464163e18e3558d6aac855a83c6
IgotChuHomie <abdouneoussama592@gmail.com>
2022-04-05 14:14:15 +0000

    adding adress model and one to one relationship with customer

--- store/models.py
+++ store/models.py
@@ -39,7 +39,9 @@ class Order(models.Model) :
     placed_at = models.DateTimeField(auto_now_add=True)
     payment_status = models.CharField(max_length=1,choices=PAYMENT_STATUS,default=PAYMENT_PENDING)
     
-
     
-    
+class Address(models.Model) :
+    street = models.CharField(max_length=255)
+    city = models.CharField(max_length=255)
+    customer = models.OneToOneField(Customer,on_delete=models.CASCADE,primary_key=True)
     
